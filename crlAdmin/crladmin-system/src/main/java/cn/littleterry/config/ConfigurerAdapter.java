55155177370ddf9d83fedc618348eed6e6d0167a
terry <tengxing7452@163.com>
2019-07-12 00:08:28 +0800

    序列化方式改为fastjson

--- crlAdmin/crladmin-system/src/main/java/cn/littleterry/config/ConfigurerAdapter.java
+++ crlAdmin/crladmin-system/src/main/java/cn/littleterry/config/ConfigurerAdapter.java
@@ -2,23 +2,18 @@ package cn.littleterry.config;
 
 import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.serializer.ToStringSerializer;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.math.BigInteger;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,7 +42,7 @@ public class ConfigurerAdapter implements WebMvcConfigurer {
      * 如不想全局 直接添加注解 @JsonSerialize(using= ToStringSerializer.class) 到相应的字段上
      * @param converters
      */
-    @Override
+    /*@Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
 
         MappingJackson2HttpMessageConverter jackson2HttpMessageConverter =
@@ -63,8 +58,13 @@ public class ConfigurerAdapter implements WebMvcConfigurer {
         converters.add(jackson2HttpMessageConverter);
         converters.add(new StringHttpMessageConverter(Charset.forName("UTF-8")));
     }
-
-    /*@Override
+*/
+    /**
+     * 解决Long 类型在 前端精度丢失的问题.
+     * 如不想全局 直接添加注解 @JSONField(deserializeUsing = ToStringSerializer.class) 到相应的字段上
+     * @param converters
+     */
+    @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         for (int i = converters.size() - 1; i >= 0; i--) {
             if (converters.get(i) instanceof MappingJackson2HttpMessageConverter) {
@@ -73,13 +73,19 @@ public class ConfigurerAdapter implements WebMvcConfigurer {
         }
         FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
         FastJsonConfig config = new FastJsonConfig();
+
+        SerializeConfig serializeConfig = SerializeConfig.getGlobalInstance();
+        serializeConfig.put(Long.class,ToStringSerializer.instance);
+        serializeConfig.put(Long.TYPE,ToStringSerializer.instance);
+
+        config.setSerializeConfig(serializeConfig);
         config.setSerializerFeatures(
                 SerializerFeature.WriteMapNullValue,
-                SerializerFeature.WriteNullListAsEmpty,
-                SerializerFeature.WriteNullStringAsEmpty,
+                //SerializerFeature.WriteNullListAsEmpty,
+                //SerializerFeature.WriteNullStringAsEmpty,
                 SerializerFeature.WriteNullNumberAsZero,
                 SerializerFeature.WriteDateUseDateFormat,
-                *//*禁用循环引用*//*
+                /*禁用循环引用*/
                 SerializerFeature.DisableCircularReferenceDetect
         );
 
@@ -91,7 +97,7 @@ public class ConfigurerAdapter implements WebMvcConfigurer {
 
         converters.add(fastJsonHttpMessageConverter);
 
-    }*/
+    }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/").setCachePeriod(0);
