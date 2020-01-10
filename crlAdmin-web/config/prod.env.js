ed280fd42d21f28a73bff055a487670e9e4e2d5c
terry <tengxing7452@163.com>
2020-01-10 20:40:26 +0800

    api

--- crlAdmin-web/config/prod.env.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  BASE_API: '"http://119.29.111.225:8000"'
-}
--- crlAdmin/crladmin-common/src/main/java/cn/littleterry/aspect/LimitType.java
+++
@@ -1,7 +0,0 @@
-package cn.littleterry.aspect;
-
-public enum LimitType {
-    CUSTOMER,
-//     by ip addr
-    IP;
-}
